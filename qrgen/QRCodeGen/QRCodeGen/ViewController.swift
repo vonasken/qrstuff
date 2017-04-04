b8a040b8bd2f79439b143f222debe972aa0f1869
Marc Von Asken <vonasken@gmail.com>
2017-04-03 18:33:25 -0700

    Fininshed Generator

--- qrgen/QRCodeGen/QRCodeGen/ViewController.swift
+++ qrgen/QRCodeGen/QRCodeGen/ViewController.swift
@@ -9,17 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    var qrcodeImage: CIImage!
 
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var btnAction: UIButton!
+    
+    @IBOutlet weak var imgQRCode: UIImageView!
 
+    @IBOutlet weak var slider: UISlider!
+
+    @IBAction func performButtonAction(sender: AnyObject) {
+        if qrcodeImage == nil {
+            if textField.text == "" {
+                return
+            }
+        }
+        
+        let data = textField.text?.data(using: String.Encoding.isoLatin1, allowLossyConversion: false)
+        
+        let filter = CIFilter(name: "CIQRCodeGenerator")
+        filter?.setValue(data, forKey: "inputMessage")
+        filter?.setValue("Q", forKey: "inputCorrectionLevel")
+        
+        qrcodeImage = filter?.outputImage
+        
+        imgQRCode.image = UIImage(ciImage: qrcodeImage)
+        
+        textField.resignFirstResponder()
+        
+    }
+    
+    
+    @IBAction func changeImageViewScale(sender: AnyObject) {
+       
+    }
 }
 
